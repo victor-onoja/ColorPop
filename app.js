00d9193479e5749ada090a056b44bbd610d3160c
victor-onoja <odohvictor47@gmail.com>
2023-01-18 18:22:36 +0100

    typescript

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-const btn = document.getElementById('btn');
-const text = document.querySelector('.text');
-
-
-function getColorsNumber(){
+"use strict";
+// export const __esModule = true;
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var btn = document.getElementById('btn');
+var text = document.querySelector('.text');
+function getColorsNumber() {
     return Math.floor(Math.random() * colors.length);
 }
-
-btn.addEventListener('click', function(){
-const colorNumber = getColorsNumber();
-document.body.style.backgroundColor = colors[colorNumber];
-text.textContent = colors[colorNumber];
+btn.addEventListener('click', function () {
+    var colorNumber = getColorsNumber();
+    document.body.style.backgroundColor = colors[colorNumber];
+    text.textContent = colors[colorNumber];
 });
-
--- hex.js
+++ hex.js
@@ -1,18 +1,16 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f' ];
-
-const btn = document.getElementById('btn');
-const text = document.querySelector('.text');
-
-function getHexNumber(){
+"use strict";
+// exports.__esModule = true;
+var hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+var btn = document.getElementById('btn');
+var text = document.querySelector('.text');
+function getHexNumber() {
     return Math.floor(Math.random() * hex.length);
 }
-
-btn.addEventListener('click', function(){
-    let hexColor = '#';
-    for (let i = 0; i < 6; i++) {
-         hexColor += hex[getHexNumber()];
+btn.addEventListener('click', function () {
+    var hexColor = '#';
+    for (var i = 0; i < 6; i++) {
+        hexColor += hex[getHexNumber()];
     }
-
     text.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
-})
+});
